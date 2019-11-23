521b43629489823a33058a2a52908064734dea4a
Sergio Rosa <scarlosjazz@gmail.com>
2019-11-23 18:42:30 -0500

    ReverseString

---
+++ ReverseString.java
@@ -0,0 +1,17 @@
+class ReverseString {
+    public static void main(String[] args) {
+
+        String str = "Hello World";
+
+        String s[] = str.split("");
+
+        for (int i = s.length - 1; i >= 0; i--) {
+
+            if (i != 0) {
+                System.out.print(s[i] + "");
+            } else {
+                System.out.println(s[i]);
+            }
+        }
+    }
+}
